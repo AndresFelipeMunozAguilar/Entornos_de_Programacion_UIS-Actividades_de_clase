4a2be86100f096d8f020fb3f8d2571217a2527f1
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-02-13 22:10:33 -0500

    añadir la capacidad de poner tu nombre en el saludo, a través de una path variable

--- Ejercicio-12_02_2024/demo/src/main/java/com/example/demo/DummyController.java
+++ Ejercicio-12_02_2024/demo/src/main/java/com/example/demo/DummyController.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class DummyController {
 
-    @RequestMapping("/greetings")
-    public String helloWorld() {
-        return "Hello World!";
+    @RequestMapping("/greetings/{name}")
+    public String helloWorld(@PathVariable String name) {
+        return "Hello, " + name;
     }
 
 }
