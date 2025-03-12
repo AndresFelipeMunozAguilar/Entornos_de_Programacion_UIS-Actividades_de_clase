aff800233db360928719af9a2debe64eaf84594a
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-03-12 15:23:25 -0500

    añadir repositorios que implementan el jparepository

---
+++ Proyecto3-ClasesEnlazadas/demo/src/main/java/com/example/demo/Clase_12Mar025/TipoDocumentoRepositorio.java
@@ -0,0 +1,7 @@
+package com.example.demo.Clase_12Mar025;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoDocumentoRepositorio extends JpaRepository<TipoDocumento, Long> {
+
+}
