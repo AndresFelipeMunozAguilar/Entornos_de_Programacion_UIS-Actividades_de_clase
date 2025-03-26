1181320d103c2ec0ee96925fc555d6b91baa8072
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-03-26 14:20:07 -0500

    crear logindto

---
+++ Proyecto3-ClasesEnlazadas/demo/src/main/java/com/example/demo/Clase_26Mar2025/LogInDTO.java
@@ -0,0 +1,24 @@
+package com.example.demo.Clase_26Mar2025;
+
+public class LogInDTO {
+
+    private String nombreUsuario;
+    private String password;
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
